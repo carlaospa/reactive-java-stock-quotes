964c8626edfc8ad2e680acae1786ea340994a6d1
carlaospa <carlaospa@gmail.com>
2021-10-03 12:34:00 -0300

    Subindo Aplicação

---
+++ src/test/java/com/carlaospa/reactive/stockquotesapi/StockQuotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlaospa.reactive.stockquotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockQuotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
